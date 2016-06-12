a27d065e2567069e2a80f0ec3fe20b923a33fc6f
George Chkhvirkia <georgechkhvirkia@gmail.com>
2016-06-12 14:15:35 +0400

    added plain api calls

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,26 +1,77 @@
 class HotelsController < ApplicationController
-  def index
-    response = HTTParty.get('https://evening-cliffs-68697.herokuapp.com/api/hotels')
+  before_action :set_hotel, except: [:index, :new, :create]
 
-    render json: response
-    # @hotels = [1,2,3]
+  def index
+    @hotels = HTTParty.get('https://evening-cliffs-68697.herokuapp.com/api/hotels?query=' + params[:query])
   end
 
   def new
+    @accomodation_types = accomodations
   end
 
   def create
+    result = HTTParty.post('https://evening-cliffs-68697.herokuapp.com/api/hotels',
+            body: hotel_params.to_json,
+            headers: { 'Content-Type' => 'application/json' } )
+
+    if result['errors'].present?
+      @accomodation_types = accomodations
+      @errors = result['errors']
+      render 'new'
+    else
+      redirect_to hotels_path
+    end
   end
 
   def show
   end
 
   def edit
+    @accomodation_types = accomodations
   end
 
   def update
+    result = HTTParty.put('https://evening-cliffs-68697.herokuapp.com/api/hotels/' + params[:id],
+            body: hotel_params.to_json,
+            headers: { 'Content-Type' => 'application/json' } )
+
+    if result['errors'].present?
+      @accomodation_types = accomodations
+      @errors = result['errors']
+      render 'edit'
+    else
+      redirect_to hotels_path
+    end
   end
 
   def destroy
+    result = HTTParty.delete('https://evening-cliffs-68697.herokuapp.com/api/hotels/' + params[:id])
+
+    if result['errors'].present?
+      flash[:alert] = result['errors'].first
+    end
+
+    redirect_to hotels_path
+  end
+
+private
+
+  def accomodations
+    HTTParty.get('https://evening-cliffs-68697.herokuapp.com/api/accomodation_types')
+  end
+
+  def set_hotel
+    @hotel = HTTParty.get('https://evening-cliffs-68697.herokuapp.com/api/hotels/' + params[:id])
+  end
+
+  def hotel_params
+    {
+      hotel: {
+        name: params[:name],
+        address: params[:address],
+        star_rating: params[:star_rating],
+        accomodation_type_id: params[:accomodation_type_id]
+      }
+    }
   end
 end
